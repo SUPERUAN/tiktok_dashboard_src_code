985f4d2fe30682c1254d86bef14a688e7bcd20a4
Mayson Ross Kieng <mayson.kieng@infomeis.com>
2026-03-11 17:44:59 +0700

    generate tiktok project in backend

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TiktokController } from './tiktok/tiktok.controller';
+import { TiktokService } from './tiktok/tiktok.service';
+import { TiktokModule } from './tiktok/tiktok.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TiktokModule],
+  controllers: [AppController, TiktokController],
+  providers: [AppService, TiktokService],
 })
 export class AppModule {}
